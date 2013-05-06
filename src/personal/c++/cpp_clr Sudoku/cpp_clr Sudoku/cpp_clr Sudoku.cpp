f29d628488afa7cb8c7af8d43818af66b08ff12b
Jonathan Chandler <chandlerjonathan@ymail.com>
2013-05-06 15:12:57 -0400

    win7 loses files every 30 minutes.  help, github!

--- src/personal/c++/cpp_clr Sudoku/cpp_clr Sudoku/cpp_clr Sudoku.cpp
+++ src/personal/c++/cpp_clr Sudoku/cpp_clr Sudoku/cpp_clr Sudoku.cpp
@@ -6,6 +6,27 @@ using namespace System;
 
 int main(array<System::String ^> ^args)
 {
-    Console::WriteLine(L"Hello World");
+    //Console::WriteLine(L"Hello World");
+
+	/*
+	cliext::vector<int> test(9);
+	
+	printf("before:  %i\n", test.at(0));
+
+	test.at(0) = 99;
+	
+	printf("after:  %i\n", test.at(0));
+	*/
+
+	// Menu
+	printf("'q' to exit\n");
+
+	System::String^ line;
+	do{
+		line = Console::ReadLine();
+	}
+	while(!line->Equals("q"));
+
+
     return 0;
 }
